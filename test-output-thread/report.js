faa2d11a40aeea2f87618edabad81223620c681d
saikiran <saikiran@gmail.com>
2023-12-01 22:59:06 +0530

    first comment

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"e483e293-137f-4207-84aa-1559f9c07651","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1701417108683,"group":1,"content":"","tags":"","end":1701417171764,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
